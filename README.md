f7d4761880c6c46089377af46ad28a1d3b66f667
Tomasz Muras <nexor1984@gmail.com>
2017-03-05 19:16:04 +0100

    Remove USE and CREATE lines from mysqldump files

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ mysqldump-filter
 ================
 
 Utility to process mysqldump created dumps.
+
+
+mysqldump-edit
+================
+
+Use it to remove "CREATE DATABASE ..." and "USE ..." lines from your MySQL database dump in efficient way.
+Those instructions are added to the dump when --databases or --all-databases option is used.
+Script is optimized for edition of huge (e.g. GBs big) mysqld dumps. Instead of removing the lines
+ it will overwrite them with spaces, so file save operiation should be quick. 
+ 
--- mysqldump-edit.php
+++ mysqldump-edit.php
@@ -1,25 +1,55 @@
 #!/usr/bin/php
 <?php
+define('HEADER_BYTES', 2000);
 
-$handle = fopen($argv[1],'c+b');
-if(!$handle) {
-
+$options = getopt("n");
+$dryRun = false;
+if($argv[1] == '-n') {
+    $dryRun = true;
+    $file = $argv[2];
+} else {
+    $file = $argv[1];
 }
+
+if(!$file) {
+    die("File not provided\n");
+}
+
+
+$handle = fopen($file,'r+b');
+if(!$handle) {
+    die("Can't open file '{$argv[1]}'\n'");
+}
+
 // Read the header - 2k bytes is more than enough
-$header = fread($handle,2000);
+$header = fread($handle, HEADER_BYTES);
 
 // Find CREATE DATABASE ... line
-$start = strpos($header,"CREATE DATABASE ");
-$stop = strpos($header,';',$start);
+removeLine($header, $handle, "CREATE DATABASE ", $dryRun);
 
-fseek($handle,$start);
-$createLine = fread($handle, $stop-$start+1);
-$overwrite = str_repeat(' ',$stop-$start+1);
-fseek($handle,$start);
-fwrite($handle,$overwrite);
-
-var_dump($createLine);
-
+echo "\n";
 // Find USE ... line
+removeLine($header, $handle, "USE ", $dryRun);
 
+function removeLine($string, $file, $match, $dryRun) {
+    $start = strpos($string, $match);
+    if($start !== false) {
+        $stop = strpos($string, ';', $start);
+
+        fseek($file, $start);
+        $createLine = fread($file, $stop - $start + 1);
+        $overwrite = str_repeat(' ', $stop - $start + 1);
+
+        if($dryRun) {
+            echo "String to overwrite at position $start:\n";
+            echo "-->$createLine<--\n";
+        } else {
+            fseek($file, $start);
+            fwrite($file, $overwrite);
+        }
+    } else {
+        echo "$match statement not found in first " . HEADER_BYTES . " bytes\n";
+    }
+
+}
 //echo $header;
